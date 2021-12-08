44de7a2057d25747e1e45f8934a84346ecf7caae
Juan Antonio Romero Molero <romero.r4l@gmail.com>
2021-12-08 16:41:06 +0100

    deploy config

---
+++ deploy.sh
@@ -0,0 +1,7 @@
+# ssh ubuntu@217.182.68.221
+cd english-platform
+git pull
+sudo docker-compose stop web
+sudo docker-compose up -d web
+cd client
+REACT_APP_BACKEND_URL=http://217.182.68.221:3000 yarn build
