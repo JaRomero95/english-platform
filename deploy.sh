f35dbd8781b21c404271eb89d1d93f86f2711137
Juan Antonio Romero Molero <romero.r4l@gmail.com>
2021-12-11 21:51:11 +0100

    Changes to deploy script

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,7 @@
 git pull
 sudo docker-compose stop web
 sudo docker-compose up -d web
+sudo docker-compose run web rails db:migrate
 cd client
 REACT_APP_BACKEND_URL=http://217.182.68.221:3000 yarn build
 cd ..
